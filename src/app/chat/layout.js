87fe8f5b7526f6168add38586bcad79e7b023735
nkonstantinidis <155967966+nkonstantinidis@users.noreply.github.com>
2025-03-11 12:56:52 +0200

    layout for diffente pages

---
+++ src/app/chat/layout.js
@@ -0,0 +1,9 @@
+// Server component for metadata
+export const metadata = {
+    title: 'Live Chat | Realtime Collaboration App',
+    description: 'Live Chat',
+  };
+  
+  export default function ChatLayout({ children }) {
+    return children;
+  }
---
+++ src/app/notes/layout.js
@@ -0,0 +1,9 @@
+// Server component for metadata
+export const metadata = {
+    title: 'Notes | Realtime Collaboration App',
+    description: 'Notes',
+  };
+  
+  export default function NotesLayout({ children }) {
+    return children;
+  }
